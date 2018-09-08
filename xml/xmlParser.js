a6d4c37f70fbce847355c012419d3f0fce07f48b
legendhl <legendhl@gmail.com>
2018-09-08 18:04:37 +0800

    xml tokenizer

--- xml/xmlParser.js
+++ xml/xmlParser.js
@@ -22,13 +22,16 @@ function InputStream(input) {
     function eof() {
         return peek() === '';
     }
-    function croak(msg){
+    function croak(msg) {
         throw new Error(`${msg} at ${line}:${col}`);
     }
 }
 
 function Tokenizer(input) {
     let stream = InputStream(input);
+    let stack = [];
+    let cur = null;
+    let curTag = null;
     return {
         peek,
         next,
@@ -38,10 +41,20 @@ function Tokenizer(input) {
     function isWhiteSpace(ch) {
         return /\s/.test(ch);
     }
+    function isChar(ch) {
+        return /\w/.test(ch);
+    }
     function readNext() {
         readWhile(isWhiteSpace);
-        if (stream.peek() === '<') {
-            return readTagName();
+        let ch = stream.peek();
+        if (ch === '<') {
+            return readTagStart();
+        } else if (ch === '>') {
+            stream.next();
+            curTag = null;
+            return readNext();
+        } else if (curTag !== null) {
+            return readTagAttr();
         } else {
             return readTagValue();
         }
@@ -53,23 +66,59 @@ function Tokenizer(input) {
         }
         return str;
     }
-    function readTagName() {
-
+    function readTagName(suffix) {
+        let str = readWhile(isChar);
+        curTag = str;
+        return { type: `tagName${suffix}`, value: str };
+    }
+    function readTagAttr() {
+        readWhile(isWhiteSpace);
+        let key = readWhile(isChar);
+        expectChar('=');
+        expectChar('"');
+        let value = readWhile(ch => /[^"]/.test(ch));
+        expectChar('"');
+        return { type: 'tagAttr', value: { [key]: value } };
+    }
+    function expectChar(ch) {
+        if (ch !== stream.next()) {
+            // TODO: not correct pos
+            croak();
+        }
+        return ch;
     }
     function readTagValue() {
-        
+        let value = readWhile(ch => /[^<]/.test(ch));
+        if (!value) {
+            return null;
+        }
+        return { type: 'tagValue', value: value };
+    }
+    function readTagStart() {
+        expectChar('<');
+        if (stream.peek() === '/') {
+            return readTagEnd();
+        } else {
+            return readTagName('Start');
+        }
+    }
+    function readTagEnd() {
+        stream.next();
+        return readTagName('End');
     }
     function peek() {
-
+        return cur || (cur = readNext());
     }
     function next() {
-        
+        let tok = cur;
+        cur = null;
+        return tok || readNext();
     }
     function eof() {
         return peek() === null;
     }
     function croak() {
-        
+        stream.croak(`Unexpected char ${stream.peek()}`);
     }
 }
 
@@ -80,7 +129,7 @@ const xml = `
         <count unit="word">360000</count>
     </note>
     <other>
-        <node name="attr" key="what" id="8"/>
+        <node name="attr" key="what" id="8"></node>
     </other>
 </root>
 `;
