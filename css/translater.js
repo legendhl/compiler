17cfa686e5ce790d3c2388c21951b79ad354431d
legendhl <legendhl@gmail.com>
2018-09-23 12:44:51 +0800

    style translater

--- css/translater.js
+++ css/translater.js
@@ -2,18 +2,44 @@ const Css = require('./css');
 const ReactStyle = require('./react');
 
 function css2React(css) {
-
+    let ast = Css.css2AST(css);
+    ast = ast.map(item => {
+        if (item.type === 'Selector') {
+            item.type = 'ClassName';
+            item.val = item.val.replace(/-\w/g, match => match.slice(1).toUpperCase()).slice(1);
+            item.declarations = item.declarations.map(declaration => {
+                declaration.val.property = declaration.val.property.replace(/-\w/g, match => match.slice(1).toUpperCase());
+                // TODO declaration.val.value
+                return declaration;
+            })
+        }
+        return item;
+    });
+    return ReactStyle.ast2React(ast);
 }
 
 function react2Css(style) {
-
+    let ast = ReactStyle.react2AST(style);
+    ast = ast.map(item => {
+        if (item.type === 'ClassName') {
+            item.type = 'Selector';
+            item.val = '.' + item.val.replace(/[A-Z]/g, match => '-' + match.toLowerCase());
+            item.declarations = item.declarations.map(declaration => {
+                declaration.val.property = declaration.val.property.replace(/[A-Z]/g, match => '-' + match.toLowerCase());
+                // TODO declaration.val.value
+                return declaration;
+            })
+        }
+        return item;
+    });
+    return Css.ast2CSS(ast);
 }
 
 const style = `
-* {
+.all {
     box-sizing: border-box;
 }
-body {
+.container {
     color: #000;
     background: #fff;
     padding: 0;
@@ -24,6 +50,7 @@ body {
 }
 `;
 
+console.log(css2React(style));
 // let ast = Css.css2AST(style);
 // console.log(ast);
 // console.log(Css.ast2CSS(ast));
@@ -46,3 +73,4 @@ absolute: {
 // ast = ReactStyle.react2AST(reactStyle);
 // console.log(ast);
 // console.log(ReactStyle.ast2React(ast))
+console.log(react2Css(reactStyle));
