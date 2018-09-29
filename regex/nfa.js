036457491f685e0b26d760fdcebf51d84d229584
legendhl <legendhl@gmail.com>
2018-09-29 15:43:53 +0800

    add regexp match

--- regex/nfa.js
+++ regex/nfa.js
@@ -20,7 +20,6 @@ class State {
     }
 }
 
-
 function post2nfa(str) {
     let stack = [];
     const matchState = new State(MATCH);
@@ -65,6 +64,42 @@ function post2nfa(str) {
     return state;
 }
 
-let str = 'abb.+.a.';
-let state = post2nfa(str);
-console.log(state);
+function match(state, str) {
+    let curList = [];
+    let nextList = [];
+
+    addState(curList, state);
+    for (let c of str) {
+        step(curList, c, nextList);
+        [curList, nextList] = [nextList, curList];
+    }
+    return curList.some(state => state.c === MATCH);
+}
+
+function step(curList, c, nextList) {
+    nextList.splice(0, nextList.length);
+    curList.forEach(state => {
+        if (state.c === c) {
+            addState(nextList, state.next);
+        }
+    });
+}
+
+function addState(list, state) {
+    if (!state || list.includes(state)) {
+        return;
+    }
+    if (state.c === SPLIT) {
+        addState(list, state.next);
+        addState(list, state.next2);
+    } else {
+        list.push(state);
+    }
+}
+
+function re(regexp, str) {
+    let state = post2nfa(regexp);
+    return match(state, str);
+}
+
+console.log(re('abb.+.a.', 'abba'));
